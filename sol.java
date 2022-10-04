86e6e4ec9b4775304a1477a5dd6983e943cf73f8
kiranmahendra523 <72977434+kiranmahendra523@users.noreply.github.com>
2022-10-04 13:46:31 +0530

    Update sol.java

--- sol.java
+++ sol.java
@@ -1,12 +1,12 @@
 class Solution {
-    boolean solve(TreeNode root,int targetSum,int x){
+    boolean solve(TreeNode root,int temp,int x){
         if(root==null){
             return false;
         }
-        if((targetSum+root.val)==x&&(root.left==null&&root.right==null)){
+        if((temp+root.val)==x&&(root.left==null&&root.right==null)){
             return true;
         }
-        return solve(root.left,targetSum+root.val,x)||solve(root.right,targetSum+root.val,x);
+        return solve(root.left,temp+root.val,x)||solve(root.right,temp+root.val,x);
     }
     public boolean hasPathSum(TreeNode root, int targetSum) {
         return solve(root,0,targetSum);
